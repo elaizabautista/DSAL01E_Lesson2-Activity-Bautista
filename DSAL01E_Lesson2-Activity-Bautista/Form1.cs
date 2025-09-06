42341f51c55264e294842114be33fc2c0523bf79
elaizabautista <2023-2-02500@lpunetwork.edu.ph>
2025-09-06 17:08:26 +0800

    Add project files.

---
+++ DSAL01E_Lesson2-Activity-Bautista/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSAL01E_Lesson2_Activity_Bautista
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browsebtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFile.FileName);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Bachelor of Science in Computer Science");
+            comboBox1.Items.Add("Bachelor of Science in Aeronautical Engineering");
+            comboBox1.Items.Add("Bachelor of Science in Civil Engineering");
+            comboBox1.Items.Add("Bachelor of Science in Computer Engineering");
+            comboBox1.Items.Add("Bachelor of Science in Electrical Engineering");
+            comboBox1.Items.Add("Bachelor of Science in Electronics Engineering");
+            comboBox1.Items.Add("Bachelor of Science in Mechanical Engineering");
+            comboBox1.Items.Add("Bachelor of Science in Industrial Engineering");
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Clear all textboxes
+            studentname1.Text = "";
+            studentnumber1.Text = "";
+            yearlevel1.Text = "";
+            creditunits1.Text = "";
+            scholar1.Text = "";
+            coursenumber1.Text = "";
+            coursecode1.Text = "";
+            coursedesc12.Text = "";
+            unitlecture1.Text = "";
+            unitlaboratory1.Text = "";
+            time1.Text = "";
+            day1.Text = "";
+            total1.Text = "";
+            labfee1.Text = "";
+            totaltuition1.Text = "";
+            totalmis1.Text = "";
+            cisco1.Text = "";
+            booklet1.Text = "";
+            tuitionandfee1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            coursenumberlistbox.Items.Add(coursenumber1.Text);
+            coursecodelistbox.Items.Add(coursecode1.Text);
+            coursedesclistbox.Items.Add(coursedesc12.Text);
+            unitleclistbox.Items.Add(unitlecture1.Text);
+            unitlablistbox.Items.Add(unitlaboratory1.Text);
+            creditunitlistbox.Items.Add(creditunits1.Text);
+            timelistbox.Items.Add(time1.Text);
+            daylistbox.Items.Add(day1.Text);
+
+            totallistbox.Items.Add(totaltuition1.Text);
+            totalmislistbox.Items.Add(totalmis1.Text);
+            comlablistbox.Items.Add(labfee1.Text);
+            ciscolistbox.Items.Add(cisco1.Text);
+            bookletlistbox.Items.Add(booklet1.Text);
+            totalnumunitslistbox.Items.Add(total1.Text);
+            totalfeeslistbox.Items.Add(tuitionandfee1.Text);
+
+        }
+    }
+}
---
+++ DSAL01E_Lesson2-Activity-Bautista/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DSAL01E_Lesson2-Activity-Bautista")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("DSAL01E_Lesson2-Activity-Bautista")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3598dc7b-9272-4fe3-9a26-263448df1b87")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
