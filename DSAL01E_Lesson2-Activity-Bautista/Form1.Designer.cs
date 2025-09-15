bd3f714bf1ae71d19c417d793303ccfd743fb1c5
elaizabautista <2023-2-02500@lpunetwork.edu.ph>
2025-09-16 00:30:10 +0800

    aa

--- DSAL01E_Lesson2-Activity-Bautista/Form1.Designer.cs
+++ DSAL01E_Lesson2-Activity-Bautista/Form1.Designer.cs
@@ -851,7 +851,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.MistyRose;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(1045, 1055);
             this.Controls.Add(this.totalfeeslistbox);
             this.Controls.Add(this.totalnumunitslistbox);
